db0977297bdb4297079f20c721516a0433b4531a
Summer Snow <ScoMorii@163.com>
2024-11-21 18:15:22 +0800

    区域服务批量新增

--- src/main/java/com/jzo2o/foundations/service/impl/ServeServiceImpl.java
+++ src/main/java/com/jzo2o/foundations/service/impl/ServeServiceImpl.java
@@ -1,14 +1,26 @@
 package com.jzo2o.foundations.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.ObjectUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jzo2o.common.expcetions.ForbiddenOperationException;
 import com.jzo2o.common.model.PageResult;
+import com.jzo2o.foundations.enums.FoundationStatusEnum;
+import com.jzo2o.foundations.mapper.RegionMapper;
+import com.jzo2o.foundations.mapper.ServeItemMapper;
 import com.jzo2o.foundations.mapper.ServeMapper;
+import com.jzo2o.foundations.model.domain.Region;
 import com.jzo2o.foundations.model.domain.Serve;
+import com.jzo2o.foundations.model.domain.ServeItem;
 import com.jzo2o.foundations.model.dto.request.ServePageQueryReqDTO;
+import com.jzo2o.foundations.model.dto.request.ServeUpsertReqDTO;
 import com.jzo2o.foundations.model.dto.response.ServeResDTO;
 import com.jzo2o.foundations.service.IServeService;
 import com.jzo2o.mysql.utils.PageHelperUtils;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author Wilson
@@ -17,6 +29,10 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ServeServiceImpl extends ServiceImpl<ServeMapper, Serve> implements IServeService {
+    @Resource
+    private ServeItemMapper serveItemMapper;
+    @Resource
+    private RegionMapper regionMapper;
     /**
      * 分页查询
      *
@@ -29,5 +45,32 @@ public class ServeServiceImpl extends ServiceImpl<ServeMapper, Serve> implements
         PageResult<ServeResDTO> serveResDTOPageResult = PageHelperUtils.
                 selectPage(servePageQueryReqDTO, () -> baseMapper.queryServeListByRegionId(servePageQueryReqDTO.getRegionId()));
         return serveResDTOPageResult;
+    }
+
+    @Override
+    public void batchAdd(List<ServeUpsertReqDTO> serveUpsertReqDTOList) {
+        for (ServeUpsertReqDTO serveUpsertReqDTO : serveUpsertReqDTOList) {
+            // 1.校验服务项是否为启用状态，不是启用状态不能新增
+            ServeItem serveItem = serveItemMapper.selectById(serveUpsertReqDTO.getServeItemId());
+            // 如果服务项信息不存在或未启用
+            if(ObjectUtil.isNull(serveItem)||serveItem.getActiveStatus()!= FoundationStatusEnum.ENABLE.getStatus()){
+                throw new ForbiddenOperationException("该服务不存在或未启用,无法添加到区域下使用");
+            }
+
+            // 2.校验是否重复新增
+            Integer count = lambdaQuery()
+                    .eq(Serve::getRegionId, serveUpsertReqDTO.getRegionId())
+                    .eq(Serve::getServeItemId, serveUpsertReqDTO.getServeItemId())
+                    .count();
+            if(count>0){
+                throw new ForbiddenOperationException(serveItem.getName()+"服务已存在");
+            }
+
+            // 3.新增服务
+            Serve serve = BeanUtil.toBean(serveUpsertReqDTO, Serve.class);
+            Region region = regionMapper.selectById(serveUpsertReqDTO.getRegionId());
+            serve.setCityCode(region.getCityCode());
+            baseMapper.insert(serve);
+        }
     }
 }
